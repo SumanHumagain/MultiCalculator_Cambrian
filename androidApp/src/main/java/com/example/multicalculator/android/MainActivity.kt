59f808e904cfc38ef624f364fa7ba4ec7c814a36
SUMAN HUMAGAIN <returnzerosh@gmail.com>
2024-06-09 20:03:01 -0400

    part two of a multi-part assignment started, added calc functions in mainactivity

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -37,4 +42,33 @@ fun DefaultPreview() {
     MyApplicationTheme {
         GreetingView("Hello, Android!")
     }
+}
+
+@Composable
+fun CalcView(){
+
+}
+
+@Composable
+fun CalcRow(){
+
+}
+
+@Composable
+fun CalcDisplay(){
+
+}
+
+@Composable
+fun CalcNumericButton(){
+
+}
+
+@Composable
+fun CalcOperationButton() {
+
+}
+@Composable
+fun CalcEqualsButton(){
+
 }
