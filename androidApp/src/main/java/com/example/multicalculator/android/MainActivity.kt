1188ff215858555ff89a255cb3d661200a1cff3e
SUMAN HUMAGAIN <returnzerosh@gmail.com>
2024-06-09 21:02:20 -0400

    Implemented logic to calculate numeric buttons

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -50,13 +53,27 @@ fun CalcView(){
 }
 
 @Composable
-fun CalcRow(){
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
 
+    Row {
+        for (i in startNum until endNum) {
+            CalcNumericButton(number = i, display = display)
+        }
+    }
 }
+
+
 
 @Composable
 fun CalcDisplay(display: MutableState<String>) {
-    Text(text = display.value)
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .height(50.dp)
+            .padding(5.dp)
+            .fillMaxWidth()
+    )
 }
 
 
